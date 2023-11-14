b587bf44585b4a3740e197e47ea98bc9bf015176
zahrabateninia <zahrabatenin@gmail.com>
2023-11-14 11:16:28 +0330

    Get the values of book's information

--- library.js
+++ library.js
@@ -1,4 +1,8 @@
-const myLibrary = []; // All of your book objects are going to be stored in it
+const addBookBtn = document.querySelector(".addBookBtn");
+const dialog = document.querySelector("dialog");
+const removeBtn = document.querySelector("remove");
+const addBtn = document.querySelector("add");
+const myLibrary = []; 
 
 function Book() {
   // the constructor...
@@ -7,35 +11,22 @@ function Book() {
 function addBookToLibrary() {
   // do stuff here
 }
-// Write a function that loops through the array and displays each book on the page
 
 
-// Add a few books to your array so you can see the display.
-
-
-
-
-// Add a “NEW BOOK” button that brings up a form allowing users 
-// to input the details for the new book: author, title, number of pages, whether it’s been read 
-const addBookBtn = document.querySelector(".addBookBtn");
-const dialog = document.querySelector("dialog");
-const removeBtn = document.querySelector("remove");
-const addBtn = document.querySelector("add");
-// show the dialog
 addBookBtn.addEventListener("click", () => {
-  dialog.showModal()
+  dialog.setAttribute("open", 'true');
 });
 
+addBtn.addEventListener('click', function(event){
+  event.preventDefault;
+})
+// get the info about the new book
+const bookTitle = document.getElementById("bookTitle").value;
+const bookAuthor = document.getElementById("bookAuthor").value;
+const pages = document.getElementById("pages").value;
+const haveRead = document.getElementById("haveRead").checked;
+const notRead = document.getElementById("notRead").checked;
 
 
 
-// Add a button on each book’s display to remove the book from the library.
-// You will need to associate your DOM elements with the actual book objects in some way. 
-// One easy solution is giving them a data-attribute that corresponds to the index of the library array.
 
-
-
-
-// Add a button on each book’s display to change its read status.
-// To facilitate this you will want to create the function that toggles 
-// a book’s read status on your Book prototype instance
