1236432821c37959318c77a48cc684f18c593dbd
zahrabateninia <zahrabatenin@gmail.com>
2023-12-11 16:10:41 +0330

    Convert the Book constructor function to a class

--- library.js
+++ library.js
@@ -1,10 +1,12 @@
 const myLibrary = []; 
 
-function Book(title, author, pages, readStatus){
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.readStatus = readStatus;
+class Book {
+  constructor(title, author, pages, readStatus){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+  }
 };
 
 function addBookToLibrary(){
